5792fc3bfb6e85c739c8354159f2535c738b354e
saufiroja <saufiroja08@gmail.com>
2022-02-20 19:47:57 +0800

    delete

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ require('./db/models/sequelize');
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
 };
 
 app.use(morgan('dev'));
